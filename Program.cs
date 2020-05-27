e6bfb886fd74e3ffdb4372828a7b699843f8ec2c
Thiago Gonçalves Novaes <thiago.novaes@bunzlbrasil.protcapad.com.br>
2020-05-27 19:04:29 -0300

    Second Version

--- Program.cs
+++ Program.cs
@@ -11,26 +11,34 @@ namespace MediaTurma
         static void Main(string[] args)
         {
             int turma;
-            int qtdalunos;
 
             Console.Write("Insira a quantidade de turmas existentes: ");
             turma = Convert.ToInt32(Console.ReadLine());
 
-            Console.Clear();
+            double totalalunos = SaberTotalAlunos(turma);
+
+            Console.WriteLine("A média de alunos de cada turma é de: " + totalalunos + " alunos");
+
+            Console.ReadLine();
+
+        }
+        public static double SaberTotalAlunos(int turma)
+        {
 
             int[] listaqtdealunos = new int[turma];
+            int qtdalunos;
 
             for (int i = 0; i < turma; i++)
             {
                 Console.WriteLine("Quantas alunos tem na turma " + (i + 1) + ":");
                 qtdalunos = Convert.ToInt32(Console.ReadLine());
 
-                if(qtdalunos <= 40)
+                if (qtdalunos <= 40)
                 {
                     listaqtdealunos[i] = qtdalunos;
                 }
                 else
-                { 
+                {
                     Console.WriteLine("A quantidade de alunos informados não condiz com o total " +
                         "estabalecido de no máximo 40 alunos por turma");
 
@@ -45,16 +53,12 @@ namespace MediaTurma
                         Console.Clear();
                     }
                     listaqtdealunos[i] = qtdalunos;
-                }               
+                    
+                }
+                
             }
-
-            var totalalunos = listaqtdealunos.Average();
-
-            Console.Clear();
-
-            Console.WriteLine("A média de alunos de cada turma é de: " + totalalunos + "alunos");
-
-            Console.ReadLine();
+            double totalalunos = listaqtdealunos.Average();
+            return totalalunos;
 
         }
     }
